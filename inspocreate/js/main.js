6b541eb0eb5c2e917a21332929937618f80ea672
bilalarshad24k <bilalarshad24k@gmail.com>
2021-01-22 12:20:27 +0500

    Js file changed

--- inspocreate/js/main.js
+++ inspocreate/js/main.js
@@ -1,3 +1,5 @@
+// NOTE: The section of code below changes the style of categories slider when clicked 
+
 var x = document.querySelectorAll(".categItem");
 for (let i = 0; i < x.length; i++) {
     x[i].addEventListener('click', styleCategItem)
@@ -15,6 +17,7 @@ function styleCategItem(event) {
 }
 
 
+//This  Section of code below somehow designs the Tab and defines how the style must change when user click on each tab 
 
 var y = document.querySelectorAll('.tab');
 for (let i = 0; i < y.length; i++) {
@@ -22,11 +25,10 @@ for (let i = 0; i < y.length; i++) {
 }
 
 function assignActivTabClass(event) {
+
     if (event.currentTarget.classList.contains("in-activ-tab")) {
         event.currentTarget.classList.add("activ-tab")
-    } else if (event.currentTarget.classList.contains('activTab')) {
-        event.currentTarget.classList.remove("activ-tab")
-        event.currentTarget.classList.add("in-activ-tab")
+    } else {
 
     }
 }
